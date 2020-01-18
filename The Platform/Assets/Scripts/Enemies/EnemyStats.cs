a11db9b599a12048bda7483259901f631206d6ad
gkaragoz <gkaragoz96@gmail.com>
2020-01-18 15:45:47 +0300

    Create EnemyStats scriptable objects.

---
+++ The Platform/Assets/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+    [Header("Initialization")]
+    [SerializeField]
+    private EnemyStats_SO _enemyDefinition_Template = null;
+
+    [Header("Debug")]
+    [SerializeField]
+    private EnemyStats_SO _enemy = null;
+
+    #region Initializations
+
+    private void Awake() {
+        if (_enemyDefinition_Template != null) {
+            _enemy = Instantiate(_enemyDefinition_Template);
+        }
+    }
+
+    #endregion
+
+    #region Reporters
+
+    public string GetName() {
+        return _enemyDefinition_Template.Name;
+    }
+
+    public float GetSpawnRate() {
+        return _enemy.SpawnRate;
+    }
+
+    public int GetCrashScore() {
+        return _enemy.CrashScore;
+    }
+
+    public float GetHideTime() {
+        return _enemy.HideTime;
+    }
+
+    #endregion
+
+}
---
+++ The Platform/Assets/Scripts/Enemies/EnemyStats_SO.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy Stats", menuName = "Scriptable Objects/Enemy Stats")]
+public class EnemyStats_SO : ScriptableObject {
+
+    #region Properties
+
+    [SerializeField]
+    private string _name = "Enemy";
+
+    [SerializeField]
+    private float _spawnRate = 0;
+
+    [SerializeField]
+    private int _crashScore = 0;
+
+    [SerializeField]
+    private float _hideTime = 0f;
+
+    public string Name {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public float SpawnRate {
+        get { return _spawnRate; }
+        set { _spawnRate = value; }
+    }
+    public int CrashScore {
+        get { return _crashScore; }
+        set { _crashScore = value; }
+    }
+    public float HideTime { 
+        get { return _hideTime; }
+        set { _hideTime = value; }
+    }
+
+    #endregion
+
+}
