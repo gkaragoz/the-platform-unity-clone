e458d9ca057782591e063832b479809316c19434
gkaragoz <gkaragoz96@gmail.com>
2020-01-18 15:47:21 +0300

    Create Enemy.cs and ICrashableObject.cs scripts

---
+++ The Platform/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [Header("Debug")]
+    [SerializeField]
+    [Utils.ReadOnly]
+    private EnemyStats _enemyStats = null;
+
+    public EnemyStats EnemyStats { 
+        get {
+            return _enemyStats;
+        }
+    }
+
+    private void Start() {
+        _enemyStats = GetComponent<EnemyStats>();
+    }
+
+    public void OnCrashed() {
+        Invoke("Hide", _enemyStats.GetHideTime());
+
+        int crashScore = _enemyStats.GetCrashScore();
+        GameManager.instance.AddScoreToPlayer(crashScore);
+
+        Debug.Log(_enemyStats.name + " crashed and got " + crashScore);
+    }
+
+    private void Hide() {
+        this.gameObject.SetActive(false);
+    }
+
+}
