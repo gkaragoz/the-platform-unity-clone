ce9a2f7d27d565e145425fdebe9948a5a616e9d7
gkaragoz <gkaragoz96@gmail.com>
2020-01-18 09:55:09 +0300

    Create LeanBlade.cs script and Blade scene to implement basics of Blade animations.

---
+++ The Platform/Assets/LeanBlade.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanBlade : MonoBehaviour {
+
+    [Header("Initializations")]
+    [SerializeField]
+    private Animator _animator = null;
+    [SerializeField]
+    private float _time = 2f;
+    [SerializeField]
+    private Transform _destinationTransform = null;
+
+    private const string PLACEMENT_ANIMATION_TRIGGER = "PLACEMENT_ANIMATION";
+    private const string DISAPPEAR_ANIMATION_TRIGGER = "DISAPPEAR_ANIMATION";
+
+    private bool _isMoving = false;
+    private bool _hasPlayedDisappearAnimation = false;
+
+    private void Update() {
+        if (HasDestination() && !HasDestinationReached() && _isMoving == false) {
+            this.gameObject.SetActive(true);
+            _hasPlayedDisappearAnimation = false;
+
+            PlayPlacementAnimation();
+            MoveToDestination();
+        } else if (HasDestination() && HasDestinationReached() && _hasPlayedDisappearAnimation == false) {
+            PlayDisappearAnimation();
+            _isMoving = false;
+        }
+    }
+
+    private bool HasDestination() {
+        return _destinationTransform == null ? false : true;
+    }
+
+    private bool HasDestinationReached() {
+        return Mathf.Abs(transform.position.z - _destinationTransform.position.z) <= 0.001f ? true : false;
+    }
+
+    private void PlayPlacementAnimation() {
+        _animator.SetTrigger(PLACEMENT_ANIMATION_TRIGGER);
+    }
+
+    private void PlayDisappearAnimation() {
+        _animator.SetTrigger(DISAPPEAR_ANIMATION_TRIGGER);
+        _hasPlayedDisappearAnimation = true;
+    }
+
+    private void MoveToDestination() {
+        _isMoving = true;
+        LeanTween.move(this.gameObject, new Vector3(transform.position.x, transform.position.y, _destinationTransform.position.z), _time).setEase(LeanTweenType.easeInOutQuad);
+    }
+
+    public void SetActiveFalse() {
+        this.gameObject.SetActive(false);
+    }
+
+}
