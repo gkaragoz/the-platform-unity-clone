7263a2ce43accbf08224a0a0a3f275dee2bbbe91
gkaragoz <gkaragoz96@gmail.com>
2020-01-18 13:06:39 +0300

    Implement PanelCountdownMenu.cs script and UI.

---
+++ The Platform/Assets/Scripts/UI/PanelCountdownMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class PanelCountdownMenu : MonoBehaviour {
+
+    [Header("Initializations")]
+    [SerializeField]
+    private TextMeshProUGUI _txtCountdown = null;
+    [SerializeField]
+    private GameObject _gameplayMenu = null;
+
+    private int _countdown = 3;
+
+    private IEnumerator ICountdown() {
+        while (true) {
+            _txtCountdown.text = _countdown.ToString();
+            _countdown--;
+         
+            yield return new WaitForSeconds(1f);
+
+            if (_countdown <= 0) {
+                break;
+            }
+        }
+
+        OpenGamePlayMenu();
+        HideThisMenu();
+    }
+
+    private void OpenGamePlayMenu() {
+        _gameplayMenu.SetActive(true);
+    }
+
+    private void HideThisMenu() {
+        this.gameObject.SetActive(false);
+    }
+
+    public void StartCountdown() {
+        StartCoroutine(ICountdown());
+    }
+
+}
